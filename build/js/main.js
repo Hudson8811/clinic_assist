f4d5ee41cf62033ce3b67c6f42f66bab115bc4dc
hudson8811 <hudson88@yandex.ru>
2021-03-04 19:55:33 +0300

    changed

--- build/js/main.js
+++ build/js/main.js
@@ -29,4 +29,21 @@ tpl:'<div class="fancybox-share"><h1>{{SHARE}}</h1><p><a class="fancybox-share__
 $(document).ready(function(){
     $('.new_b-slider').slick();
     $('.new_specialists-slider').slick();
+    $('.new_reviews-slider').slick();
+    $('.new_team-slider').slick({
+        slidesToShow:4,
+        slidesToScroll:1,
+        infinite:false,
+    });
+
+    $('[data-fancybox]').fancybox({
+		touch: false,
+		scrolling: 'no',
+		beforeShow: function(){
+			$("body").css({'overflow-y':'hidden'});
+		},
+		afterClose: function(){
+			$("body").css({'overflow-y':'visible'});
+		}
+	});
 })
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,4 +1,21 @@
 $(document).ready(function(){
     $('.new_b-slider').slick();
     $('.new_specialists-slider').slick();
+    $('.new_reviews-slider').slick();
+    $('.new_team-slider').slick({
+        slidesToShow:4,
+        slidesToScroll:1,
+        infinite:false,
+    });
+
+    $('[data-fancybox]').fancybox({
+		touch: false,
+		scrolling: 'no',
+		beforeShow: function(){
+			$("body").css({'overflow-y':'hidden'});
+		},
+		afterClose: function(){
+			$("body").css({'overflow-y':'visible'});
+		}
+	});
 })
