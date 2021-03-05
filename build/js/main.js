b7059f611b1a0fa40df31507504c9939c80fb580
hudson8811 <hudson88@yandex.ru>
2021-03-05 18:50:04 +0300

    changed

--- build/js/main.js
+++ build/js/main.js
@@ -27,13 +27,35 @@ tpl:'<div class="fancybox-share"><h1>{{SHARE}}</h1><p><a class="fancybox-share__
 /* my scripts */
 
 $(document).ready(function(){
-    $('.new_b-slider').slick();
-    $('.new_specialists-slider').slick();
-    $('.new_reviews-slider').slick();
+    $('.new_b-slider').slick({
+        infinite:false,
+    });
+    $('.new_specialists-slider').slick({
+        infinite:false,
+    });
+    $('.new_reviews-slider').slick({
+        infinite:false,
+    });
     $('.new_team-slider').slick({
         slidesToShow:4,
         slidesToScroll:1,
         infinite:false,
+        responsive: [
+            {
+              breakpoint: 999,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              },
+          ]
     });
 
     $('[data-fancybox]').fancybox({
@@ -46,4 +68,29 @@ $(document).ready(function(){
 			$("body").css({'overflow-y':'visible'});
 		}
 	});
+
+    $('.new_text_block-mobile-link').click(function(e){
+        e.preventDefault();
+        $(this).hide();
+        $('.new_text_block-smarl_mobile p').show();
+    });
+
+    $('.new_treatment-block-heading').click(function(e){
+        e.preventDefault();
+
+        if($(window).width() < 701){
+            const $this = $(this);
+            const $item = $(this).parent('.new_treatment-block');
+            const $text = $this.siblings('.new_text_block-texts');
+            if(!$item.hasClass('is-active')){
+                $('.new_treatment-block').removeClass('is-active');
+                $('.new_text_block-texts').slideUp(200);
+                $item.addClass('is-active');
+                $text.slideDown(200);
+            } else{
+                $item.removeClass('is-active');
+                $text.slideUp(200);
+            }
+        }
+    })
 })
--- src/js/partials/main.js
+++ src/js/partials/main.js
@@ -1,11 +1,33 @@
 $(document).ready(function(){
-    $('.new_b-slider').slick();
-    $('.new_specialists-slider').slick();
-    $('.new_reviews-slider').slick();
+    $('.new_b-slider').slick({
+        infinite:false,
+    });
+    $('.new_specialists-slider').slick({
+        infinite:false,
+    });
+    $('.new_reviews-slider').slick({
+        infinite:false,
+    });
     $('.new_team-slider').slick({
         slidesToShow:4,
         slidesToScroll:1,
         infinite:false,
+        responsive: [
+            {
+              breakpoint: 999,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1,
+                }
+              },
+          ]
     });
 
     $('[data-fancybox]').fancybox({
@@ -18,4 +40,29 @@ $(document).ready(function(){
 			$("body").css({'overflow-y':'visible'});
 		}
 	});
+
+    $('.new_text_block-mobile-link').click(function(e){
+        e.preventDefault();
+        $(this).hide();
+        $('.new_text_block-smarl_mobile p').show();
+    });
+
+    $('.new_treatment-block-heading').click(function(e){
+        e.preventDefault();
+
+        if($(window).width() < 701){
+            const $this = $(this);
+            const $item = $(this).parent('.new_treatment-block');
+            const $text = $this.siblings('.new_text_block-texts');
+            if(!$item.hasClass('is-active')){
+                $('.new_treatment-block').removeClass('is-active');
+                $('.new_text_block-texts').slideUp(200);
+                $item.addClass('is-active');
+                $text.slideDown(200);
+            } else{
+                $item.removeClass('is-active');
+                $text.slideUp(200);
+            }
+        }
+    })
 })
